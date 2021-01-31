ff7ecedac171723dca2a4c646e581b31914454a6
Lucia <lucia.aznar1987@gmail.com>
2021-01-31 18:48:01 -0300

    actualizacion de clase recargaPay.js

--- cypress/cypress/integration/examples/recargaPay.js
+++ cypress/cypress/integration/examples/recargaPay.js
@@ -1,10 +1,17 @@
 
 describe('Grupo de Pruebas Front End Web RecargaPay', function(){
+    beforeEach(()=>{
+        cy.fixture('dataClient.json').as('userData')
+        })
+
     it('Prueba Recarga celular',function(){
+    cy.get('@userData').then((userData)=>{
+
     cy.visit('https://recargapay.com.br/');
     cy.contains( 'Recarga de celular' ).click();
-    cy.get('#allNumberInput').type("1234586456");
+    cy.get('#allNumberInput').type(userData.prefijoNumber + userData.restNumber);
     cy.get('#showModalButton').click()
-    cy.get('#showNumber').should('contain.text', '(12) 34586456')
+    cy.get('#showNumber').should('contain.text','('+userData.prefijoNumber+') '+userData.restNumber)
     })
     })
+})
